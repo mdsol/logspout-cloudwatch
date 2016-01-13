64a4776f741158becfa3e2c0bb9976567f415a32
Benton Roberts <broberts@mdsol.com>
2016-01-12 17:44:06 -0800

    remove spurious module

--- modules.go
+++ modules.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/benton/logspout-cloudwatch"
-	_ "github.com/gliderlabs/logspout/adapters/cloudwatch"
 	_ "github.com/gliderlabs/logspout/adapters/raw"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/httpstream"
