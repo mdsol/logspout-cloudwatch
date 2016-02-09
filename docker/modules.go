c091913fe71b1f4156032ef4b31a0234ad2a4194
Benton Roberts <broberts@mdsol.com>
2016-02-09 08:47:25 -0800

    adjust license and source URL

--- docker/modules.go
+++ docker/modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/benton/logspout-cloudwatch"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/routesapi"
+	_ "github.com/mdsol/logspout-cloudwatch"
 )
