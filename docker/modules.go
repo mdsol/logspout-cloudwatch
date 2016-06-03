909dfaf659a6f7493d2a0a17a380720f2f8fe267
Benton Roberts <broberts@mdsol.com>
2016-06-03 09:45:44 -0700

    match log group name exactly when searching AWS

--- docker/modules.go
+++ docker/modules.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/routesapi"
 	_ "github.com/mdsol/logspout-cloudwatch"
